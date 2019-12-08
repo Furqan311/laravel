954f344cea10739ee1d03b31fc3de6a557ffb0c6
Furqan311 <22428696+Furqan311@users.noreply.github.com>
2019-12-08 23:40:16 +0500

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=mozilla">
+    <title>GoldenGigs</title>
+    <link rel="stylesheet" href="{{url('/global.css')}}" />
+</head>
+<body>
+
+    <div>
+
+    <div class="header">
+    <h2>GoldenGigs</h2>
+
+
+    
+    <ul>
+        <li  class="selected"><a href="/home">Home</a></li>
+        <li><a href="/privacyandterms">Privacy & Terms</a></li>
+    </ul>
+    
+    
+    </div>
+
+    <div class="bodyContainer">
+    <div class="btns">
+    <button class="fb">Facebook</button>
+    <button class="yt">Youtube</button>
+    </div>
+    </div>
+
+
+   </div>
+
+
+</body>
+</html>
---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=mozilla">
+    <title>GoldenGigs</title>
+    <link rel="stylesheet" href="{{url('/global.css')}}" />
+</head>
+<body>
+    <div>
+
+    <div class="header">
+    <h2>GoldenGigs</h2>
+
+    
+    <ul>
+        <li><a href="/home">Home</a></li>
+        <li class="selected"><a href="/privacyandterms">Privacy & Terms</a></li>
+    </ul>
+    
+    
+    </div>
+
+
+    <div class="privacy">
+        <div class="privacyheader">
+            <h2>Privacy Policy and Terms</h2>
+        </div>
+
+        <div class="privacybody">
+            <div class="privacycontents">
+                <p>
+Privacy Policy for GoldenGigs app on Facebook is being developed with the intensions keep in mind to keep the Facebook and its data secure, it is our first priority and no compromises will be considered against this intension.
+The purpose of this document is to enclose the affidavit and the relevant data that could highlight the basic terms of demand for the grant of user_likes permission for goldengigs.herokuapp.com.
+It is to declare that the use of Facebook API is Solely for the Education purposes:
+    <br><br>Highlights:<br><br>
+•	To learn the integration of Different APIs in one Application. <br>
+•	To learn how REST end points works.<br>
+•	How different APIs accept Requests to exaggerate data and respond to Client after successful authentication<br>
+•	How Auth works in different Models<br><br>
+Project Scopes:<br><br>
+•	Project is considered to be developed to serve the user with update of diferent Pages liked and their live stats.<br>
+•	Different APIs are being used to integrate into application.<br>
+•	Other Social media options (Twitter, Facebook, YouTube) would be get available right at one place.<br>
+<br>
+Developer Background : <br><br>
+	Name: Furqan Hameed<br>
+	Student of University of Gujrat (UOG), Pakistan<br>
+	Contact (official): 17074198-091@uog.edu.pk <br><br><br>
+
+<span class="note"><i>Note: The purpose of this Application is Solely for Education. We have major concern with Facebook and its security to make it safer place for everyone. It is our responsibility to make it safer.</i></span>
+</p>
+            </div>
+        </div>
+
+    </div>
+    
+    
+    </div>
+</body>
+</html>
